7cf02a35db8b86ca8f172a9ce684fc4b83a94493
abrham <getnetabrham@gmail.com>
2024-02-01 15:20:01 +0300

    model created and migrated

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,70 @@
+import uuid
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class Genre(models.Model):
+    name = models.CharField(max_length=100, unique=True, help_text='Enter a book genre (e.g. Science Fiction)')
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('genre-detail', args=[str(self.id)])
+    
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField(null=True, blank=True)
+
+    class Meta:
+        ordering = ['last_name', 'first_name']
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('author-detail', args=[str(self.id)])
+    
+class Language(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.RESTRICT, related_name = 'books')
+    summary = models.TextField(max_length=1000)
+    isbn = models.CharField('ISBN', max_length=13, unique=True)
+    genre = models.ManyToManyField(Genre)
+    language = models.ForeignKey(Language, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('book-detail', args=[str(self.id)])
+    
+class BookInstance(models.Model):
+    LOAN_STATUS = (
+        ('m', 'Maintenance'),
+        ('o', 'On loan'),
+        ('a', 'Available'),
+        ('r', 'Reserved'),
+    )
+    
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID for this particular book across whole library')
+    book = models.ForeignKey(Book, on_delete=models.RESTRICT, null=True)
+    imprint = models.CharField(max_length=200)
+    due_back = models.DateField(null=True, blank=True)
+    status = models.CharField( max_length=1, choices=LOAN_STATUS, blank=True, default='m', help_text='Book availability', )
+
+    class Meta:
+        ordering = ['due_back']
+
+    def __str__(self):
+        return f'{self.book.title} ({self.id})'
+    
